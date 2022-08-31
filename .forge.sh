61ce5d88b547fcd86aebd0f553a045b96903968c
James Brooks <james@alt-three.com>
2022-08-31 15:55:52 +0100

    Create .forge.sh

---
+++ .forge.sh
@@ -0,0 +1,14 @@
+cd $FORGE_SITE_PATH
+
+echo "Using .forge.sh script..."
+
+git pull origin $FORGE_SITE_BRANCH
+
+$FORGE_COMPOSER install --no-interaction --prefer-dist --optimize-autoloader
+
+( flock -w 10 9 || exit 1
+    echo \'Restarting FPM...\'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
+    
+if [ -f artisan ]; then
+    $FORGE_PHP artisan migrate --force
+fi
